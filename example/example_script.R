30cd5bf4a821078295f3854f5379c9f61e69cf16
Nolan Young Zabala <64682145+youngnolan1@users.noreply.github.com>
2024-08-19 15:21:13 -0400

    Update example_script.R with more detailed steps

--- example/example_script.R
+++ example/example_script.R
@@ -3,14 +3,19 @@
 # Load package
 library(wbfirmadaptation)
 
-# Prep data - edit data_directory and survey arguments
+# MANDATORY FIRST STEP: Prep data - edit data_directory and survey arguments
 reg_data <- wbfirmadaptation::prep_reg_data("path/to/WB/geolinked/master/data", "Survey")
 
-# Climate on firm - edit level_or_dviation and output_directory
+
+# Climate on firm regressions - edit level_or_deviation and output_directory
+
+# Export summary charts for heat days, temperature, and temperature volatility
 wbfirmadaptation::climate_on_firm_regs_charts(reg_data, "level", "path/where/output/saved")
+
+# Export regression tables
 wbfirmadaptation::climate_on_firm_regs_tables(reg_data, "level", "path/where/output/saved")
 
-# Interactions - edit firm_outcome, climate_var, firm_characteristics, and output_directory
+# Climate interacted with firm characteristics regressions - edit firm_outcome, climate_var, firm_characteristics, and output_directory
 wbfirmadaptation::interaction_reg_table(reg_data, "log(sales)", "heat_days",
                                         c("young", "small", "obstacle_accesstofinance"),
                                         "path/where/output/saved/table_name.html")
